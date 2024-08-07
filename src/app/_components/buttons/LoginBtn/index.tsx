7ae025ddf28eb1bac8d1745030a4d872fd7f671c
BenNormann <benosmailbox@gmail.com>
2024-08-07 09:57:13 -0700

    blank login button file added

---
+++ src/app/_components/buttons/LoginBtn/index.tsx
@@ -0,0 +1,8 @@
+export default function LoginBtn() {
+    // const { isLoggedIn } = useContext(AuthContext);
+    // const [wasConnectModalOpen, setWasConnectModalOpen] = useState(false);
+    // const [isDropdownHidden, setIsDropdownHidden] = useState(true);
+    return(
+        <></>
+    )
+}
