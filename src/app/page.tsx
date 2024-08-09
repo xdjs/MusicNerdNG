9ab224571e588bfd720d4387170804d0172dfcad
BenNormann <benosmailbox@gmail.com>
2024-08-08 17:12:33 -0700

    carousel template with pictures and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ const HomePage = () => {
     <div className="bg-jellygreen flex flex-col items-center w-full min-h-screen">
       <div className="flex flex-col items-center w-full">
         <h1 className="py-10 text-4xl text-white my-4">Go deep on artists</h1>
-        <Carousel direction="forward" speed={2}/>
-        <Carousel direction="backward" speed={2}/>
+        <Carousel direction="forward" speed={1}/>
+        <Carousel direction="backward" speed={1}/>
       </div>
     </div>
   );
