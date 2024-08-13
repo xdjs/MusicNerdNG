7118357affc141e8b4c6c1ff33b88d0370bcebd1
BenNormann <benosmailbox@gmail.com>
2024-08-13 15:28:27 -0700

    changed env for test

--- src/utils/getInfo.js
+++ src/utils/getInfo.js
@@ -1,6 +1,6 @@
 import queryString from 'querystring';
 import axios from 'axios'
-import { SPOTIFY_WEB_CLIENT_ID, SPOTIFY_WEB_CLIENT_SECRET } from "@/env"
+import { SPOTIFY_WEB_CLIENT_ID, SPOTIFY_WEB_CLIENT_SECRET } from "../env"
 
 const getWiki = async (id) => {
   try {
