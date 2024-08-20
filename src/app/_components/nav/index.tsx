0696c8b46f5f1d13065708f2ecfea0271c9cc4cf
BenNormann <benosmailbox@gmail.com>
2024-08-20 13:35:04 -0700

    fixed nav bar padding and home text padding

--- src/app/_components/nav/index.tsx
+++ src/app/_components/nav/index.tsx
@@ -4,17 +4,19 @@ import SearchBar from "../searchBar"
 
 export default function Nav() {
     return (
-        <nav className="nav-bar grow py-5 flex items-center gap-x-4">
+        <nav className="nav-bar grow flex items-center">
             <Link href={"/"}>
                 <img src="/musicNerdLogo.png" className="w-24" alt="logo" />
             </Link>
-            <div className="grid nav-grid grow gap-x-4 gap-y-4">
+            <div className="grow"></div>
+            {/* add nav-grid back in when login and add button implemented */}
+            {/* <div className="grid nav-grid grow gap-x-4 gap-y-4">  */}
                 <div className="SearchBar">
                     <SearchBar/>
                 </div>
-                <button className="pink-btn">Artist +</button>
-                <LoginBtn/>
-            </div>
+                {/* <button className="pink-btn">Artist +</button>
+                <LoginBtn/> */}
+            {/* </div> */}
         </nav>
     )
 }
