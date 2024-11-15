a7a274f138d1936c4798d501745d551bd7ee2eb8
jaesul <jaesukpro@gmail.com>
2024-11-14 16:03:20 -0800

    added whitelist user management

--- src/app/admin/whitelisted-data-table.tsx
+++ src/app/admin/whitelisted-data-table.tsx
@@ -1,20 +1,16 @@
 "use client"
-
 import {
     ColumnDef,
     SortingState,
     flexRender,
     getCoreRowModel,
-    getPaginationRowModel,
     getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table";
-
-import { approveUgcAdmin } from "@/server/utils/queriesTS";
+import { addUsersToWhitelist } from "@/server/utils/queriesTS";
 import { useRouter } from "next/navigation";
-
 import { Button } from "@/components/ui/button";
-
+import { removeFromWhitelist } from "@/server/utils/queriesTS";
 import {
     Table,
     TableBody,
@@ -22,15 +18,66 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-} from "@/components/ui/table"
-import { useState } from "react"
+} from "@/components/ui/table";
+import { useEffect, useState } from "react";
+import { X } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import SearchBar from "./UserSearch";
+
+
+export function AddWhitelistDialog() {
+    const router = useRouter();
+    const [users, setUsers] = useState<string[]>([]);
+    const [uploadStatus, setUploadStatus] = useState<{ status: "success" | "error", message: string, isLoading: boolean }>({ status: "success", message: "", isLoading: false });
+
+    async function addToWhitelist() {
+        setUploadStatus({ status: "success", message: "", isLoading: true });
+        const resp = await addUsersToWhitelist(users);
+        if (resp.status === "success") router.refresh();
+        setUploadStatus({ status: resp.status as "success" | "error", message: resp.message, isLoading: false });
+    }
+
+    function setUserWithFilter(user: string) {
+        setUsers([...users.filter((u) => u !== user), user]);
+    }
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">Add Users to Whitelist</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px] text-black">
+                <DialogHeader>
+                    <DialogTitle>Add Users to Whitelist</DialogTitle>
+                </DialogHeader>
+                <div className="space-y-4">
+                    <SearchBar setUsers={(user:string) => setUserWithFilter(user)} />
+                    <div>
+                        {users.map((user) => <Button variant="outline" key={user}>{user} <X className="w-4 h-4 ml-1" /></Button>)} 
+                    </div>
+                </div>
+                <DialogFooter>
+                    <Button type="submit" onClick={() => addToWhitelist()}>Save changes {uploadStatus.isLoading ? <img className="w-4 h-4" src="/spinner.svg" alt="whyyyyy" /> : ""}</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+}
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
     data: TData[]
 }
 
-export function DataTable<TData, TValue>({
+export default function WhitelistedDataTable<TData, TValue>({
     columns,
     data,
 }: DataTableProps<TData, TValue>) {
@@ -53,22 +100,24 @@ export function DataTable<TData, TValue>({
 
     type TDataWithId = TData & { id: string };
 
-    async function approveSelected() {
-        const selectedUGC = table.getFilteredSelectedRowModel().rows.map((row) => row.original as TDataWithId).map((row) => row.id);
+    async function commitRemoveFromWhitelist() {
+        const selectedUsers = table.getFilteredSelectedRowModel().rows.map((row) => row.original as TDataWithId).map((row) => row.id);
         setUploadStatus({ status: "success", message: "", isLoading: true });
-        const resp = await approveUgcAdmin(selectedUGC);
-        setUploadStatus({ status: resp.status as "success" | "error", message: resp.message, isLoading: false });
-        if (resp.status === "success") router.refresh();
-        
+        await removeFromWhitelist(selectedUsers);
+        setUploadStatus({ status: "success", message: "", isLoading: false });
+        router.refresh();
     }
-
 
     return (
         <div className="space-y-4">
-            <Button variant="outline" onClick={() => approveSelected()} className="w-fit text-black">
-                {uploadStatus.isLoading ? <img className="w-4 h-4" src="/spinner.svg" alt="whyyyyy" />
-                    : "Approve Selected"}
-            </Button>
+            <div className="flex gap-4 text-black">
+                <Button variant="outline" onClick={() => commitRemoveFromWhitelist()}>
+                    {uploadStatus.isLoading ? <img className="w-4 h-4" src="/spinner.svg" alt="whyyyyy" />
+                        : "Remove Selected from Whitelist"}
+                </Button>
+                <AddWhitelistDialog />
+            </div>
+
             {uploadStatus.status === "error" && <p className="text-red-500">{uploadStatus.message}</p>}
             <div className="rounded-md border border-black bg-gray-700">
                 <Table >
