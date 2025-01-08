9b587834cb46c3ccd82cc7b181befbc0198ab3a4
jaesul <jaesukpro@gmail.com>
2025-01-08 14:01:27 -0800

    fixed env variables

--- src/server/db/drizzle.ts
+++ src/server/db/drizzle.ts
@@ -1,8 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema'
+import { SUPABASE_DB_CONNECTION } from '@/env';
 
-const connectionString = process.env.DATABASE_URL ?? "postgresql://postgres.kyhlkqriyvevjqtufidu:carlloveshentai123@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
+const connectionString = SUPABASE_DB_CONNECTION ?? "postgresql://postgres.kyhlkqriyvevjqtufidu:carlloveshentai123@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
 
 // Disable prefetch as it is not supported for "Transaction" pool mode 
 const client = postgres(connectionString, { prepare: false })
