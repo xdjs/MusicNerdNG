89a1c04aad6b00bbe2d55c0b4fbe7e062e45fb28
jaesul <jaesukpro@gmail.com>
2025-01-08 17:11:43 -0800

    revert db hard code

--- src/server/db/drizzle.ts
+++ src/server/db/drizzle.ts
@@ -4,7 +4,7 @@ import * as schema from './schema'
 import { SUPABASE_DB_CONNECTION } from '@/env';
 
         
-const connectionString ="postgresql://postgres.cbabvmebugudeuylronz:CarlAgressivlyLovesHentai123@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
+const connectionString = SUPABASE_DB_CONNECTION
 
 // Disable prefetch as it is not supported for "Transaction" pool mode 
 const client = postgres(connectionString, { prepare: false })
