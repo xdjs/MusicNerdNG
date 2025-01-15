4c171b5c7b9f4438871a4f423da12ec4ae2587de
jaesul <jaesukpro@gmail.com>
2025-01-15 15:57:38 -0800

    fixing error with no whitelisted users

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -24,7 +24,7 @@ export default async function Admin() {
             </div>
             <div>
                 <h2 className="text-xl pb-3">Whitelisted Users</h2>
-                <WhitelistedDataTable columns={whitelistedColumns} data={whitelistedUsers} />
+                <WhitelistedDataTable columns={whitelistedColumns} data={whitelistedUsers || []} />
             </div>
         </section>
     );
