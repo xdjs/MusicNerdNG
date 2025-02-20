c6045a67277e4b76870a02af6aac5bdffaf93981
jaesul <jaesukpro@gmail.com>
2025-02-19 18:08:22 -0800

    cleanup for buenos aries

--- src/app/_components/ArtistLinks.tsx
+++ src/app/_components/ArtistLinks.tsx
@@ -6,7 +6,7 @@ import { Session } from "next-auth";
 
 function PlatformLink({ link, descriptor, image }: { link: string, descriptor: string, image: string }) {
     return (
-        <li className={`mb-5 list-none`}>
+        <li className={`list-none`}>
             <Link href={`${link}`} target="blank" className="text-black">
                 <div className="link-item-grid gap-x-4 corners-rounded">
                     <img className="mr-3" src={image} alt="" height={50} width={50} />
