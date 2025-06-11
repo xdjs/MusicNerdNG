4b95a69e2a5536dc1c7b6408adedb1249cf67da0
Carl <clt@artificialbits.org>
2025-06-11 14:18:37 -0700

    Remove unused chat components

--- src/app/artist/[id]/page.tsx
+++ src/app/artist/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getArtistById, getAllLinks, getArtistLinks } from "@/server/utils/queriesTS";
+import { getArtistById, getArtistLinks } from "@/server/utils/queriesTS";
 import { getSpotifyImage, getArtistWiki, getSpotifyHeaders, getNumberOfSpotifyReleases, getArtistTopTrack } from "@/server/utils/externalApiQueries";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import { Spotify } from 'react-spotify-embed';
@@ -8,9 +8,6 @@ import Link from "next/link";
 import AddArtistData from "./_components/AddArtistData";
 import { getServerAuthSession } from "@/server/auth";
 import { notFound } from "next/navigation";
-import Dashboard2 from "./_components/Dashboard2";
-import LLMChat from "./_components/LLMChat";
-import getAiResponse from "@/server/utils/AiBro";
 
 type ArtistProfileProps = {
     params: { id: string };
@@ -32,19 +29,9 @@ export default async function ArtistProfile({ params, searchParams }: ArtistProf
         getArtistWiki(artist.wikipedia ?? ""),
         getArtistLinks(artist),
         getArtistTopTrack(artist.spotify ?? "", headers),
-        // getAiResponse(`Give me a 230 characterbio of the artist ${JSON.stringify(artist)} be casual and focus on web3 only if they are a web3 artist don't add any extra details of how the composition was made`)
     ]);
 
-    // return (
-    //     <section className="relative block md:grid md:grid-cols-2 items-center justify-center py-5 px-4 gap-8 auto-rows-fr max-w-[1000px] mx-auto">
-    //         <div>
-    //             <Dashboard2 bio={aiResponse.response} artist={artist} img={spotifyImg.artistImage} session={session} availableLinks={allLinks} isOpenOnLoad={opADM === "1"} />
-    //         </div>
-    //         <div className="hidden md:block">
-    //             <LLMChat artist={artist} />
-    //         </div>
-    //     </section>
-    // )
+
 
     return (
         <>
