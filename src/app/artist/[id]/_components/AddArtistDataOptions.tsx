c69c57cbf054fac073b735a4d80ce7c0ac033775
Max Lee <maxtacular@icloud.com>
2025-06-23 13:58:00 -0700

    Renamed "Options" button to "Tips" and added back all link formats

--- src/app/artist/[id]/_components/AddArtistDataOptions.tsx
+++ src/app/artist/[id]/_components/AddArtistDataOptions.tsx
@@ -11,12 +11,20 @@ import {
 import { UrlMap } from "@/server/db/DbTypes";
 
 export default function AddArtistDataOptions({availableLinks, setOption}: {availableLinks: UrlMap[], setOption: (option: string) => void}) {
-    const dataOptions = availableLinks.map(link => <DropdownMenuItem key={link.id} className="cursor-pointer" onClick={() => setOption(link.example.split(" ")[0])}>{link.example.split(" ")[0]}</DropdownMenuItem>);
+    const dataOptions = availableLinks.map(link => (
+        <DropdownMenuItem
+            key={link.id}
+            className="cursor-pointer"
+            onClick={() => setOption(link.example)}
+        >
+            {link.example}
+        </DropdownMenuItem>
+    ));
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button className="bg-pastypink text-white h-12 hover:bg-gray-400">
-                    Options
+                    Tips
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="max-h-48 overflow-auto scrollbar-hide" align="end">
