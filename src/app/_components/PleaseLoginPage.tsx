91c03ac9e6747a6e40b3b9475a747bc6f2e3767c
Piper-Hood <79597213+Piper-Hood@users.noreply.github.com>
2025-07-21 11:08:35 -0700

    Update button text in PleaseLoginPage component for capitalization consistency

--- src/app/_components/PleaseLoginPage.tsx
+++ src/app/_components/PleaseLoginPage.tsx
@@ -24,7 +24,7 @@ export default function PleaseLoginPage({text = "Log in to access this page"}: {
     return (
         <section className="px-10 py-5 space-y-6 flex items-center justify-center flex-col">
             <h1 className="text-2xl text-center font-bold">{text}</h1>
-            <Button className="bg-pastypink hover:bg-gray-200" onClick={handleLogin}>log in</Button>
+            <Button className="bg-pastypink hover:bg-gray-200" onClick={handleLogin}>Log In</Button>
         </section>
     )
 }
