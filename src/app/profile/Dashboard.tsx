245a0ab22813bb803d4a7e8669f8c48cc5a8498d
Piper-Hood <79597213+Piper-Hood@users.noreply.github.com>
2025-07-21 14:54:28 -0700

    Fix typo in total entries display of UgcStats component: change "out of" to "of" for improved clarity.

--- src/app/profile/Dashboard.tsx
+++ src/app/profile/Dashboard.tsx
@@ -254,7 +254,7 @@ function UgcStats({ user, showLeaderboard = true, allowEditUsername = false, sho
                                     </Badge>
                                     {totalEntries && (
                                         <>
-                                            <span className="text-xs sm:text-base px-1">out of</span>
+                                            <span className="text-xs sm:text-base px-1">of</span>
                                             <Badge className="bg-secondary text-secondary-foreground hover:bg-secondary text-base px-4 py-1">
                                                 {totalEntries}
                                             </Badge>
